b7c9c9319b7332228c4d2fe6543a445e05642e38
g0dsCookie <g0dscookie@copr.icu>
2025-06-08 09:05:16 +0200

    use same quoting style everywhere

--- sqfssync/__init__.py
+++ sqfssync/__init__.py
@@ -1,9 +1,9 @@
-doc = """SquashFS plug-in module for portage.
+doc = '''SquashFS plug-in module for portage.
 Downloads latest squashfs, unmounts old
 and mounts the new one.
-"""
+'''
 __doc__ = doc
-__version__ = "0.1.1"
+__version__ = '0.1.1'
 
 
 from portage.localization import _
@@ -11,30 +11,30 @@ from portage.sync.config_checks import CheckSyncConfig
 
 
 module_spec = {
-    "name": "sqfssync",
-    "description": doc,
-    "provides": {
-        "sqfssync-module": {
-            "name": "sqfssync",
-            "sourcefile": "sqfssync",
-            "class": "SqfsSync",
-            "functions": ["sync", "new", "exists"],
-            "func_desc": {
-                "sync": "Download latest squashfs and mount to destination",
-                "new": "Creates the new repository at the specified location",
-                "exists": "Returns a boolean of whether the specified dir " +
-                    "exists and the squashfs is mounted",
+    'name': 'sqfssync',
+    'description': doc,
+    'provides': {
+        'sqfssync-module': {
+            'name': 'sqfssync',
+            'sourcefile': 'sqfssync',
+            'class': 'SqfsSync',
+            'functions': ['sync', 'new', 'exists'],
+            'func_desc': {
+                'sync': 'Download latest squashfs and mount to destination',
+                'new': 'Creates the new repository at the specified location',
+                'exists': 'Returns a boolean of whether the specified dir ' +
+                    'exists and the squashfs is mounted',
             },
-            "validate_config": CheckSyncConfig,
-            "module_specific_options": (
-                "sync-sqfs-file",
-                "sync-sqfs-verify",
-                "sync-sqfs-signature-file",
-                "sync-sqfs-uid",
-                "sync-sqfs-gid",
-                "sync-sqfs-mode",
-                "sync-sqfs-options",
-                "sync-sqfs-tmpdir",
+            'validate_config': CheckSyncConfig,
+            'module_specific_options': (
+                'sync-sqfs-file',
+                'sync-sqfs-verify',
+                'sync-sqfs-signature-file',
+                'sync-sqfs-uid',
+                'sync-sqfs-gid',
+                'sync-sqfs-mode',
+                'sync-sqfs-options',
+                'sync-sqfs-tmpdir',
             )
         }
     }
--- sqfssync/sqfssync.py
+++ sqfssync/sqfssync.py
@@ -19,22 +19,22 @@ import urllib3
 
 
 class SqfsSync(NewBase):
-    """SquashFS sync class"""
+    '''SquashFS sync class'''
 
-    short_desc = "Downloads and mounts latest squashfs"
+    short_desc = 'Downloads and mounts latest squashfs'
 
     CHUNK_SIZE = 2048
 
     @staticmethod
     def name():
-        return "SqfsSync"
+        return 'SqfsSync'
 
     def __init__(self):
         # we don't need any external binary
         # however we need the following packages/use flags
         # sys-apps/util-linux[python]
         # dev-python/urllib3
-        super().__init__("mount", "sys-apps/util-linux")
+        super().__init__('mount', 'sys-apps/util-linux')
         self._http = urllib3.PoolManager()
 
     @property
@@ -51,13 +51,13 @@ class SqfsSync(NewBase):
     @property
     def verify_sig(self) -> bool:
         return self.repo.module_specific_options.get(
-            "sync-sqfs-verify", "yes").lower() in ("true", "yes")
+            'sync-sqfs-verify', 'yes').lower() in ('true', 'yes')
 
     @property
     def openpgp_key_path(self) -> str:
         return self.repo.module_specific_options.get(
-            "sync-openpgp-key-path",
-            "/usr/share/openpgp-keys/gentoo-release.asc")
+            'sync-openpgp-key-path',
+            '/usr/share/openpgp-keys/gentoo-release.asc')
 
     @property
     def signature_file(self) -> str:
@@ -68,16 +68,16 @@ class SqfsSync(NewBase):
     @property
     def mount_options(self) -> str:
         opts = []
-        extra = self.repo.module_specific_options.get("sync-sqfs-options")
+        extra = self.repo.module_specific_options.get('sync-sqfs-options')
         if extra:
             opts.append(extra)
-        return ",".join(opts)
+        return ','.join(opts)
 
     @property
     def tempdir(self) -> str:
-        tmpdir = self.repo.module_specific_options.get("sync-sqfs-tmpdir")
+        tmpdir = self.repo.module_specific_options.get('sync-sqfs-tmpdir')
         if not tmpdir:
-            tmpdir = os.getenv("PORTAGE_TMPDIR", "/tmp")
+            tmpdir = os.getenv('PORTAGE_TMPDIR', '/tmp')
         return tmpdir
 
     def _is_mounted(self) -> bool:
@@ -89,7 +89,7 @@ class SqfsSync(NewBase):
         try:
             ctx.umount()
         except Exception as err:
-            writemsg_level("Failed to unmount %s: %r\n" % (dir, err),
+            writemsg_level('Failed to unmount %s: %r\n' % (dir, err),
                            level=logging.ERROR, noiselevel=-1)
             return False
         return True
@@ -99,12 +99,12 @@ class SqfsSync(NewBase):
         ctx.source = source
         ctx.target = target
         ctx.options = self.mount_options
-        ctx.fstype = "squashfs"
+        ctx.fstype = 'squashfs'
 
         try:
             ctx.mount()
         except Exception as err:
-            writemsg_level("Failed to mount %s: %r\n" % (source, err),
+            writemsg_level('Failed to mount %s: %r\n' % (source, err),
                            level=logging.ERROR, noiselevel=-1)
             return False
         return True
@@ -137,41 +137,41 @@ class SqfsSync(NewBase):
                 openpgp_env._process_gpg_verify_output(out, err, True))
 
     def _fetch_signature(self) -> str:
-        writemsg_level("Fetching file signature...\n")
+        writemsg_level('Fetching file signature...\n')
         digest_uri = urllib.parse.urljoin(self.repo.sync_uri,
                                           self.signature_file)
-        r = self._http.request("GET", digest_uri)
+        r = self._http.request('GET', digest_uri)
 
         if r.status != 200:
-            writemsg_level("Could not fetch signature from %s: %s\n" % (
+            writemsg_level('Could not fetch signature from %s: %s\n' % (
                 digest_uri,
-                "Server returned %d" % r.status
+                'Server returned %d' % r.status
             ), level=logging.ERROR, noiselevel=-1)
             return
 
         openpgp_env = self._get_openpgp_env(self.openpgp_key_path)
         if openpgp_env is None:
-            writemsg_level("Failed to initialize OpenPGP environment\n",
+            writemsg_level('Failed to initialize OpenPGP environment\n',
                            level=logging.ERROR, noiselevel=-1)
             return
 
         try:
-            with open(self.openpgp_key_path, "rb") as f:
+            with open(self.openpgp_key_path, 'rb') as f:
                 openpgp_env.import_key(f)
 
-            verify_buffer = io.StringIO(r.data.decode("utf8"))
+            verify_buffer = io.StringIO(r.data.decode('utf8'))
             verified_data, signatures = self._pgp_verify(openpgp_env,
                                                          verify_buffer)
             openpgp_env.close()
         except GematoException as e:
             writemsg_level(
-                f"Failed to validate digest signature:\n{e}\n",
+                f'Failed to validate digest signature:\n{e}\n',
                 level=logging.ERROR,
                 noiselevel=-1,
             )
             return
 
-        lines = verified_data.decode("utf8").split("\n")
+        lines = verified_data.decode('utf8').split('\n')
         for line in lines:
             if not line:
                 continue
@@ -180,20 +180,20 @@ class SqfsSync(NewBase):
 
     def _download(self) -> tuple[str, bool]:
         url = urllib.parse.urljoin(self.repo.sync_uri, self.filename)
-        tmpfile = os.path.join(self.tempdir, self.filename + ".tmp")
+        tmpfile = os.path.join(self.tempdir, self.filename + '.tmp')
 
         if self.verify_sig:
             signature = self._fetch_signature()
             if not signature:
-                writemsg_level("Could not find SHA512 signature for %s\n" %
+                writemsg_level('Could not find SHA512 signature for %s\n' %
                                self.filename, level=logging.ERROR,
                                noiselevel=-1)
-                return ("", False)
+                return ('', False)
             hasher = sha512()
 
-        writemsg_level("Downloading new SquashFS file...\n")
-        r = self._http.request("GET", url, preload_content=False)
-        with open(tmpfile, "wb") as out:
+        writemsg_level('Downloading new SquashFS file...\n')
+        r = self._http.request('GET', url, preload_content=False)
+        with open(tmpfile, 'wb') as out:
             while True:
                 data = r.read(self.CHUNK_SIZE)
                 if not data:
@@ -205,31 +205,31 @@ class SqfsSync(NewBase):
 
         if self.verify_sig:
             if hasher.hexdigest() != signature:
-                writemsg_level("Signature %s does not match!\n" %
+                writemsg_level('Signature %s does not match!\n' %
                                hasher.hexdigest(),
                                level=logging.ERROR, noiselevel=-1)
-                return ("", False)
+                return ('', False)
 
         return (tmpfile, True)
 
     def exists(self) -> bool:
-        """Tests whether the directory exists and is mounted"""
+        '''Tests whether the directory exists and is mounted'''
         return os.path.exists(self.repo.location)
 
     def update(self) -> tuple[int, bool]:
-        """Download latest squashfs and remount."""
-        destfile = os.path.join(self.repo.location.rstrip("/") + ".sqfs")
-        destfile_new = destfile + ".new"
+        '''Download latest squashfs and remount.'''
+        destfile = os.path.join(self.repo.location.rstrip('/') + '.sqfs')
+        destfile_new = destfile + '.new'
         tmpfile, success = self._download()
         if not success or not tmpfile:
             return (1, False)
 
-        writemsg_level("Replacing old SquashFS with new one...\n")
+        writemsg_level('Replacing old SquashFS with new one...\n')
         try:
             shutil.move(tmpfile, destfile_new)
         except OSError as err:
-            writemsg_level("Could not move temporary file %s\n" +
-                           " to destination %s: %r\n" % (tmpfile, destfile_new,
+            writemsg_level('Could not move temporary file %s\n' +
+                           ' to destination %s: %r\n' % (tmpfile, destfile_new,
                                                          err),
                            level=logging.ERROR, noiselevel=-1)
             return (2, False)
@@ -242,14 +242,14 @@ class SqfsSync(NewBase):
             try:
                 os.remove(destfile)
             except OSError as err:
-                writemsg_level("Could not remove %s: %r\n" % (destfile, err),
+                writemsg_level('Could not remove %s: %r\n' % (destfile, err),
                                level=logging.ERROR, noiselevel=-1)
                 return (4, False)
 
         try:
             os.rename(destfile_new, destfile)
         except OSError as err:
-            writemsg_level("Could not rename %s to %s: %r\n" % (
+            writemsg_level('Could not rename %s to %s: %r\n' % (
                 destfile_new, destfile, err
             ), level=logging.ERROR, noiselevel=-1)
             return (5, False)
@@ -259,14 +259,14 @@ class SqfsSync(NewBase):
         return (os.EX_OK, True)
 
     def new(self, **kwargs) -> tuple[int, bool]:
-        """Do the initial download and install of the repository"""
+        '''Do the initial download and install of the repository'''
         if kwargs:
             self._kwargs(kwargs)
         try:
             if not os.path.exists(self.repo.location):
                 os.makedirs(self.repo.location)
                 self.logger(self.xterm_titles,
-                            "Created new directory %s" % self.repo.location)
+                            'Created new directory %s' % self.repo.location)
         except IOError:
             return (1, False)
         return self.update()
